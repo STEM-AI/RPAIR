6beb083f2cb5f3c5d461c802205081687605fe22
0127822 <am8536496@gmail.com>
2024-12-28 14:57:33 +0200

    add team profile and edit profile apis , organization profile

--- repair_backend/rapair_db/serializers/organization_serializers.py
+++ repair_backend/rapair_db/serializers/organization_serializers.py
@@ -1,20 +1,34 @@
 from rest_framework import serializers
-from ..models import Organization , OrganizationContact
+from ..models import Organization , OrganizationContact , Team
 
 
-    # id = models.AutoField(primary_key=True)
-    # name = models.CharField(max_length=255, unique=True)
-    # address = models.CharField(max_length=255)
-    # type = models.CharField(max_length=255)
+class OrganizationContactSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrganizationContact
+        fields = ['phone_number']
+
+
 
 class OrganizationSerializer(serializers.ModelSerializer):
+    contact = serializers.SerializerMethodField()
+    teams = serializers.SerializerMethodField()
     class Meta:
         model = Organization
-        fields = ['id','name','address','type']
+        fields = ['name','address','email','type','contact' , 'teams'] 
         extra_kwargs = {
             'name': {'required': True},
             'address': {'required': True},
             'type': {'required': True},
         }
 
+    def get_contact(self, obj):
+        contact_obj = OrganizationContact.objects.filter(organization=obj).first()
+        if contact_obj:
+            return OrganizationContactSerializer(contact_obj).data
+        else:
+            return None
+        
+    def get_teams(self, obj):
+        # TODO:
+        pass
     
--- repair_backend/rapair_db/urls.py
+++ repair_backend/rapair_db/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path , include
-from rapair_db.repair_db_urls import tokens_ulrs
-from rapair_db.repair_db_urls.user_urls import user_auth_urls , user_data_urls
-from rapair_db.validators import StrongPasswordValidator
+from rapair_db.repair_db_urls import (
+    user_auth_urls , user_data_urls , tokens_ulrs ,
+    organization_urls  ,
+)
+
+
+
+
 urlpatterns = [
     path('token/', include(tokens_ulrs)),
     path('user/auth/', include(user_auth_urls)),
     path('user/data/', include(user_data_urls)),
+    path('organization/', include(organization_urls)),
 
 ]
