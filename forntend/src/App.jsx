d7be6b18b1a5d96c899f2bcf36c3124064d46743
amany <amanyelfawey5@gmail.com>
2025-01-06 17:32:56 +0200

    done with home and cards

--- forntend/src/App.jsx
+++ forntend/src/App.jsx
@@ -1,20 +1,15 @@
 
 import React from 'react'
+import Home from './components/Home/Home.jsx'
+import CardSlider from './components/CardSlider/cardSlider.jsx';
+
 
 const App = () => {
   return (
     <>
-    
-
-
-
-
-
-<Home/>
+    <Home/>
+    <CardSlider/>
 </>
-
-
-  
   );
 
 }
