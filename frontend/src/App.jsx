b4bde2412a4641ed9b71bcda9b8a12ee081274b1
Mariem2Mostafa <mariem.19.1.2002@gmail.com>
2025-01-08 20:18:51 +0200

    addLogin

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
@@ -8,9 +7,7 @@ import Home from './components/Home/Home';
 
 import About from './pages/About/About'; 
 
-
 import ContactUs from './components/Contact/contactUs';
-
 
 const App = () => {
   return (
