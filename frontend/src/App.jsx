af6a94d57b832038cd3265773fcb00eb2c536b76
Mariem2Mostafa <mariem.19.1.2002@gmail.com>
2025-01-15 14:26:48 +0200

    add Edite login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,9 +7,12 @@ import Home from './components/Home/Home';
 import About from './pages/About/About'; 
 import ContactUs from './components/Contact/contactUs';
 import Competitions from './pages/Competitions/Competitions';
-import Login from './pages/Login/login';
-
+import Login from './pages/Auth/login';
+import Register from './pages/Auth/Register';
+import WelcomeAdmin from './pages/WElcome/welcomeAdmin';
 const App = () => {
+
+
   return (
     <>
     <Router>
@@ -21,6 +24,7 @@ const App = () => {
           <Route path="/about" element={<About />} />
           <Route path="/competitions" element={<Competitions/>} />
           <Route path="/login" element={<Login/>} />
+          <Route path="/register" element={<Register/>} />
         </Routes>
 
         <ContactUs />
