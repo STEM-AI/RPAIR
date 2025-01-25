c3164836859f0db1c3c61f76a1f151e532a58e40
0127822 <am8536496@gmail.com>
2025-01-25 11:23:25 +0200

    solve .select_related issue in userteamprofile , add url to get team profile for admin dashboard using team name

--- Backend/repair_backend/rapair_db/views/team_views/team_views.py
+++ Backend/repair_backend/rapair_db/views/team_views/team_views.py
@@ -1,7 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated 
-from ...permissions import IsJudgeUser
+from ...permissions import IsJudgeUser , IsSuperUser
 from rest_framework import status
 from ...serializers.team_serializers.team_data_serializers import TeamSerializer
 from ...models import Team ,Organization , Competition
@@ -40,7 +40,7 @@ class UserTeamProfileView(APIView):
             'coach',
             'members'
         )
-        .select_related('organization', 'teams')
+        .select_related('organization' , 'competition_event')
         )
         serializer = TeamSerializer(team , many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
@@ -94,4 +94,22 @@ class ListTeamsView(APIView):
         return Response(serializer.data, status=status.HTTP_200_OK)
         
 
+class TeamProfileView(APIView):
+    permission_classes = [IsSuperUser]
 
+    def get(self, request, team_name):
+        team = (
+            Team.objects
+            .filter(name=team_name)
+            .prefetch_related(
+            'sponsors', 
+            'social_media',
+            'previous_competition',
+            'coach',
+            'members'
+            )
+            .select_related('organization' , 'competition_event')
+            .first()
+            )
+        serializer = TeamSerializer(team)
+        return Response(serializer.data, status=status.HTTP_200_OK)
