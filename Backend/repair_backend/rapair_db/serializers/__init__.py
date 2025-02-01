9b67426c95797c7160b11237f819404bfeaa78b9
0127822 <am8536496@gmail.com>
2025-02-01 11:21:21 +0200

    reformating the response and get list event with top3 teams using serializer not cursor query execution

--- Backend/repair_backend/rapair_db/serializers/__init__.py
+++ Backend/repair_backend/rapair_db/serializers/__init__.py
@@ -3,5 +3,5 @@ from .organization_serializers import OrganizationSerializer , OrganizationConta
 from .team_serializers  import TeamCoachSerializer , TeamMemberSerializer ,TeamPreviousCompetitionSerializer ,TeamSerializer , TeamSocialMediaSerializer , TeamSponsorSerializer 
 from .user_serializers.user_data_serializers import UserSerializer , UserEditProfileSerializer
 from .competitions_serializers import CompetitionsSerializer 
-from .event_serializers import TeamCompetitionProfileSerializer , EventSerializer , EventGameSerializer
+from .event_serializers import TeamCompetitionProfileSerializer , EventSerializer , EventGameSerializer , EventListSerializer
 from .news_serializers import NewsSerializer
--- Backend/repair_backend/rapair_db/serializers/event_serializers.py
+++ Backend/repair_backend/rapair_db/serializers/event_serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from ..models import CompetitionEvent , Team , EventGame
 from ..serializers.team_serializers.team_member_serializers import TeamMemberSerializer
+from django.db.models import F 
 
 
 class TeamCompetitionProfileSerializer(serializers.ModelSerializer):
@@ -30,3 +31,29 @@ class EventGameSerializer(serializers.ModelSerializer):
     class Meta:
         model = EventGame
         fields = ['id' ,'team1', 'team2', 'score' , 'stage']
+
+class TopTeamsSerializer(serializers.ModelSerializer):
+    total_score = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Team
+        fields = ['id' , 'name' , 'total_score']
+
+    def get_total_score(self, obj):
+        return obj.teamwork_score + obj.interview_score + obj.eng_note_book_score
+
+class EventListSerializer(serializers.ModelSerializer):
+    top3_teams = serializers.SerializerMethodField(read_only = True)
+    class Meta:
+        model = CompetitionEvent
+        fields = ['id' ,'name', 'start_date', 'end_date', 'location' , 'top3_teams']
+
+    
+    def get_top3_teams(self, obj):
+        top3_teams = (
+            obj.teams.all()
+                .annotate(total_score=F('teamwork_score')+ F('interview_score')+F('inspect_score')+F('eng_note_book_score'))
+                .order_by('-total_score')[:3]
+        )
+        serializers = TopTeamsSerializer(top3_teams , many = True)
+        return serializers.data
