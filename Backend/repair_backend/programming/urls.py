6b1215a495518309099fb7b0c3fa27f1d684d603
0127822 <am8536496@gmail.com>
2025-06-01 16:13:32 +0300

    add question url

--- Backend/repair_backend/programming/urls.py
+++ Backend/repair_backend/programming/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import QuestionListAPIView,QuestionDetailAPIView,UploadQuestionsCSV
 
 urlpatterns = [
-    path('', QuestionListAPIView.as_view(), name='question-list'),
-    path('<int:id>/', QuestionDetailAPIView.as_view(), name='question-detail'),
+    path('questions/', QuestionListAPIView.as_view(), name='question-list'),
+    path('questions/<int:id>/', QuestionDetailAPIView.as_view(), name='question-detail'),
     path('upload-questions/', UploadQuestionsCSV.as_view(), name='upload-questions'),
 ]
