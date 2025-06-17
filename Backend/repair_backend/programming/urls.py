ae83798a01c081a3445edbad8d8d39861f4ae239
0127822 <am8536496@gmail.com>
2025-06-17 16:11:24 +0300

    get question url

--- Backend/repair_backend/programming/urls.py
+++ Backend/repair_backend/programming/urls.py
@@ -3,7 +3,7 @@ from .views import QuestionListAPIView,QuestionDetailAPIView,UploadQuestionsCSV,
 
 urlpatterns = [
     path('questions/<int:competition_event_id>/', QuestionListAPIView.as_view(), name='question-list'),
-    path('questions/<int:id>/', QuestionDetailAPIView.as_view(), name='question-detail'),
+    path('question/<int:id>/', QuestionDetailAPIView.as_view(), name='question-detail'),
     path('upload-questions/', UploadQuestionsCSV.as_view(), name='upload-questions'),
     path('number-of-questions/<int:id>/', NumberOfQuestionsUpdateView.as_view(), name='number-of-questions-update'),
 ]
