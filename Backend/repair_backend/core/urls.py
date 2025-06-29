e29afdae13f8b8d726e11b6b9a979c14d0dadaba
0127822 <am8536496@gmail.com>
2025-06-29 15:08:06 +0300

    game id url

--- Backend/repair_backend/core/urls.py
+++ Backend/repair_backend/core/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('event/schedule/<int:pk>/', ScheduleDetailView.as_view(), name='schedule_detail'),
     path('event/<str:event_name>/games/schedule/', GamesScheduleCreateView.as_view(), name='games_schedule'),
     path('event/<str:event_name>/<str:stage>/games/', GameListView.as_view(), name='game_list'),
-    path('event/<str:event_name>/<str:stage>/games/<int:team_id>/', GameIDRetrieveView.as_view(), name='game_id_retrieve'),
+    path('game-id/<int:team_id>/<int:event_id>/<str:stage>/', GameIDRetrieveView.as_view(), name='game_id_retrieve'),
 ]
