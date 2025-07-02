be04b1f53363d0ebe89677673781237cab408518
Mariem Mostafa <117760276+Mariem2Mostafa@users.noreply.github.com>
2025-07-02 11:04:21 +0000

    event id arducion flutter

--- Backend/repair_backend/rapair_db/repair_db_urls/flutter.py
+++ Backend/repair_backend/rapair_db/repair_db_urls/flutter.py
@@ -2,9 +2,9 @@ from django.urls import path
 from ..views.arduino_flutter import TeamAttachmentListView,TeamAttachmentUpdateView,TeamAttachmentRankingView,TeamScoreUpdateView,TeamAttachmentScoreListView
 
 urlpatterns = [
-    path('<str:event_name>/', TeamAttachmentListView.as_view(), name='flutter-team-attachment'),
-    path('<str:event_name>/team-attachment/<int:team_id>/', TeamAttachmentUpdateView.as_view(), name='flutter-team-attachment-update'),
-    path('<str:event_name>/rank/', TeamAttachmentRankingView.as_view(), name='flutter-team-attachment-ranking'),
-    path('<str:event_name>/score/<int:team_id>/', TeamScoreUpdateView.as_view(), name='flutter-team-attachment-score-update'),
-    path('<str:event_name>/score-list/', TeamAttachmentScoreListView.as_view(), name='flutter-team-attachment-score-list'),
+    path('<int:event_id>/', TeamAttachmentListView.as_view(), name='flutter-team-attachment'),
+    path('<int:event_id>/team-attachment/<int:team_id>/', TeamAttachmentUpdateView.as_view(), name='flutter-team-attachment-update'),
+    path('<int:event_id>/rank/', TeamAttachmentRankingView.as_view(), name='flutter-team-attachment-ranking'),
+    path('<int:event_id>/score/<int:team_id>/', TeamScoreUpdateView.as_view(), name='flutter-team-attachment-score-update'),
+    path('<int:event_id>/score-list/', TeamAttachmentScoreListView.as_view(), name='flutter-team-attachment-score-list'),
 ]
