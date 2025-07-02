1307bf4a6d90e8073aaefb46db6245dc332bac29
Mariem Mostafa <117760276+Mariem2Mostafa@users.noreply.github.com>
2025-07-02 11:30:55 +0000

    remove top 3 team from event serializer

--- Backend/repair_backend/rapair_db/serializers/competition_serializers/event_serializers.py
+++ Backend/repair_backend/rapair_db/serializers/competition_serializers/event_serializers.py
@@ -83,15 +83,15 @@ class EventListSerializer(serializers.ModelSerializer):
         model = CompetitionEvent
         fields = ['id' ,'name', 'start_date', 'end_date', 'location' , 'top3_teams','competition_name', 'organization_name']
 
-    @extend_schema_field(serializers.ListField(child=serializers.DictField()))
-    def get_top3_teams(self, obj):
-        top3_teams = (
-                TeamworkTeamScore.objects
-                .select_related('team' , 'team_competition_event')  # Fetch the related Team model
-                .filter(team__competition_event=obj)
-                .values('team', 'team__name')  # Include team name directly
-                .annotate(avg_score=Avg('score'))
-                .order_by('-avg_score')[:3]
-                )
-        serializers = TeamScoreSerializer(top3_teams , many = True)
-        return serializers.data
+    # @extend_schema_field(serializers.ListField(child=serializers.DictField()))
+    # def get_top3_teams(self, obj):
+    #     top3_teams = (
+    #             TeamworkTeamScore.objects
+    #             .select_related('team' , 'team_competition_event')  # Fetch the related Team model
+    #             .filter(team__competition_event=obj)
+    #             .values('team', 'team__name')  # Include team name directly
+    #             .annotate(avg_score=Avg('score'))
+    #             .order_by('-avg_score')[:3]
+    #             )
+    #     serializers = TeamScoreSerializer(top3_teams , many = True)
+    #     return serializers.data
