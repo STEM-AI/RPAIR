7707b0ca7a6d25bf1b62ffd1a73bfcb666dbab1e
Mariem Mostafa <117760276+Mariem2Mostafa@users.noreply.github.com>
2025-07-02 11:21:30 +0000

    log event id

--- Backend/repair_backend/programming/views/base.py
+++ Backend/repair_backend/programming/views/base.py
@@ -2,7 +2,8 @@ from rest_framework.generics import ListAPIView
 from rapair_db.models import EventGame
 from rest_framework.permissions import AllowAny
 from programming.serializers import ProgrammingRankSerializer
-
+import logging
+logger = logging.getLogger(__name__)
 
 
 class ProgrammingRankListView(ListAPIView):
@@ -13,4 +14,5 @@ class ProgrammingRankListView(ListAPIView):
     lookup_url_kwarg = 'event_id'
 
     def get_queryset(self):
+        logger.info(f"event_id {self.kwargs.get('event_id')}")
         return self.queryset.filter(event_id=self.kwargs.get('event_id'),stage='programming').order_by('-score')
