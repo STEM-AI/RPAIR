264e97e2d93fa7471f4dcd3d080b8161f034ad45
0127822 <am8536496@gmail.com>
2025-08-06 13:57:28 +0300

    add logs

--- Backend/repair_backend/vex_123/serializers/game.py
+++ Backend/repair_backend/vex_123/serializers/game.py
@@ -25,16 +25,16 @@ class GameSerializer(GamesSerializer):
     def update(self, instance, validated_data):
         # Update the instance with the validated data
         instance.score = validated_data.get('score', instance.score)
-        logger.info("instance score" , instance.score)
+        logger.info(f"instance score {instance.score}")
         instance.time_taken = validated_data.get('time_taken', instance.time_taken)
-        logger.info("instance.time",instance.time)
+        logger.info(f"instance.time {instance.time}")
         if instance.stage in ['vex_123','vex_123_manual','vex_123_coder_card','vex_123_programming']:
-            logger.info("driver score")
+            logger.info(f"driver score {instance.score}")
             instance.driver_score = instance.score
 
         instance.operation = "set_skills_game_score"
         instance.completed = True
         # Save the instance
-        logger.info("instance.operation" , instance.operation)
+        logger.info(f"instance.operation {instance.operation}")
         instance.save()
         return instance
