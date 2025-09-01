475a303ecaa2a370e82707c02d169ae91cfe0349
Mariem2Mostafa <mariem.19.1.2002@gmail.com>
2025-09-01 12:36:58 +0300

    handle last update

--- frontend/src/components/Competitions/MobileApp.jsx
+++ frontend/src/components/Competitions/MobileApp.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import img1 from "../../assets/cards/mobile.jpg";
 
 import JoinTeams from "../CometitionsComponent/Rules/JoinTeams/JoinTeams";
-import Rules from "../CometitionsComponent/Rules/RulesProgramming";
+import Rules from "../CometitionsComponent/Rules/RulesMobileApp";
 import FlutterAwardsList from "../Awards/FlutterAwards";
 import { Helmet } from "react-helmet-async";
 const MobileApp = () => {
@@ -21,20 +21,19 @@ const MobileApp = () => {
             </h2>
 
             <p className="text-gray-800 text-lg mb-8 leading-relaxed">
-              The Arduino competition is designed to inspire creativity and
-              hands-on learning through Arduino-based electronics projects.
-              Participants will work in teams using a standardized kit of
-              components to build and present interactive projects. The
-              competition is divided into two levels based on age group, with
-              increasing complexity and logic requirements
+              The Mobile App Competition aims to inspire creativity and hands-on learning
+              through the development of interactive mobile applications.
+              Participants will work in teams to design and build their apps using
+              standardized tools and technologies, and present demonstrations showcasing
+              their app's functionality. The competition is divided into levels based on age group,
+              with increasing complexity and logic requirements.
             </p>
           </div>
 
-          {/* Gallery Section */}
           <div className="md:w-3/5 flex justify-center">
             <img
-              src={img1} // Change this to your desired image
-              alt="VEX IQ Competition"
+              src={img1} 
+              alt="Mobile App Competition"
               className="w-[800px] h-[400px] object-cover rounded-lg shadow-lg"
             />
           </div>
