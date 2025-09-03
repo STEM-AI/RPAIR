5875eb5d4a29b36b3a4ef98545f10736d4d45691
Mariem2Mostafa <mariem.19.1.2002@gmail.com>
2025-09-03 15:48:08 +0300

    handle format code

--- frontend/src/hooks/Questions/FormatCode.jsx
+++ frontend/src/hooks/Questions/FormatCode.jsx
@@ -6,11 +6,11 @@ const formatSingleLineCode = (text) => {
   let code = text.substring(questionEnd + 1).trim();
 
   code = code
-    .replace(/^(\\n|;)+/, '')  
-    .replace(/\\n/g, "\n")    
-    .replace(/\\t/g, "    ") 
-    // .replace(/;/g, ";\n")      
-    .replace(/\n+/g, "\n");    
+    .replace(/^(\\n|;)+/, '')   // Remove leading \n or ;
+    .replace(/\\n/g, "\n")      // Replace literal \n with newline characters
+    .replace(/\\t/g, "    ")    // Replace literal \t with 4 spaces
+    .replace(/\n\s*/g, "\n")    // Remove any spaces after newlines
+    .replace(/\n+/g, "\n");     // Collapse multiple newlines
 
   return { question, code };
 };
